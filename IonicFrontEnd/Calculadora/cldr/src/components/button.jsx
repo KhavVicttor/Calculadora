e616fc4e31d85628d30f1ae6b870022d7dff6058
Victtor <51210201+KhavVicttor@users.noreply.github.com>
2019-08-09 15:26:34 -0300

    Add files via upload

--- IonicFrontEnd/Calculadora/cldr/src/components/button.jsx
+++ IonicFrontEnd/Calculadora/cldr/src/components/button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './button.css';
 export default props => {
-  let classes = 'button '
+  let classes = 'button'
   classes += props.operation ? 'operation': ' '
   classes += props.double ? 'double' : ' '
   classes += props.triple ? 'triple' : ' '
--- IonicFrontEnd/Calculadora/cldr/src/main/Calculator.jsx
+++ IonicFrontEnd/Calculadora/cldr/src/main/Calculator.jsx
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import './Calculator.css';
 import Button from '../components/button';
 import Display from '../components/Display';
+const inicialState = {
+  displayValue: '0', //Display começa com zero
+clearDisplay: false, //Criar uma função que zera
+operation : null, //armazenar as operações +-/*...
+value: [0, 0], //dois valores, ex: 80 e/ou 20
+currect : 0 //Qual está manipulando o 1 ou 2 valores
+}
 export default class Calculator extends Component {
+  state = { ...inicialState }
+  //Criamos o clone dentro do objeto
+  //Limpar operação AC e C
   //Demonstrar que um número foi apagado
   clearMemory() {
-    console.log('Limpar');
+    this.setState({ ...inicialState })
   }
   //Setar que o usuário colocou +-*/
   setOperation(operation) {
@@ -13,32 +23,48 @@ export default class Calculator extends Component {
   }
   //Adicionar dígito
   addDigito(n) {
-    console.log(n);
+    if(n === '.' && this.state.displayValue.includes('.')){
+      return //Se já tem ponto na calculadora, não aceite outro
+    }
+    //Display do valor zero, permanece zero
+    const clearDisplay = this.state.displayValue === '0'
+    || this.state.clearDisplay
+    /*Essa constante é para saber se o valor será limpo
+    ou permanece o estado do display */
+    const currectValue = clearDisplay ? '' : this.state.displayValue
+    //No display terá o valor digitado + o n digitado
+    const displayValue = currectValue + n
+    //Agora para mudar o display - o false e para não zerar
+    this.setState({displayValue, clearDisplay: false})
+  }
+  constructor(props) {
+    super(props);
+    this.clearMemory = this.clearMemory.bind(this);
+    this.setOperation = this.setOperation.bind(this);
+    this.addDigito = this.addDigito.bind(this);
   }
   render() {
     return (
       <div className="Calculator">
+        <Display value={this.state.displayValue}/>
         <Display value={100} />
-        <Button label="AC" />
-        <Button label="()" />
-        <Button label="%" />
-        <Button label="/" />
-        <Button label="7" />
-        <Button label="8" />
-        <Button label="9" />
-        <Button label="*" />
-        <Button label="4" />
-        <Button label="5" />
-        <Button label="6" />
-        <Button label="-" />
-        <Button label="1" />
-        <Button label="2" />
-        <Button label="3" />
-        <Button label="+" />
-        <Button label="+/-" />
-        <Button label="0" />
-        <Button label="." />
-        <Button label="=" />
+        <Button label="AC" click={this.clearMemory} triple />
+        <Button label="/" click={this.setOperation} operation />
+        <Button label="7" click={this.addDigito} />
+        <Button label="8" click={this.addDigito} />
+        <Button label="9" click={this.addDigito} />
+        <Button label="*" click={this.setOperation} operation />
+        <Button label="4" click={this.addDigito} />
+        <Button label="5" click={this.addDigito} />
+        <Button label="6" click={this.addDigito} />
+        <Button label="-" click={this.setOperation} operation />
+        <Button label="1" click={this.addDigito} />
+        <Button label="2" click={this.addDigito} />
+        <Button label="3" click={this.addDigito} />
+        <Button label="+" click={this.setOperation} operation />
+        <Button label="0" click={this.addDigito} double />
+        <Button label="." click={this.addDigito} />
+        <Button label="=" click={this.setOperation} operation />
       </div>
     );
   }
